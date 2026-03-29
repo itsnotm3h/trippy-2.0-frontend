5c7002154f781cc6684fd7d8f8845b6f5d1cba39
itsnotm3h <thewhit3pages@gmail.com>
2026-03-29 21:28:11 +0800

    style: added template for navbar

--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -1,15 +1,16 @@
-export const Navbar = ()=>{
-
-
-    return (
-        <>
+export const Navbar = () => {
+  return (
+    <>
+      <div className="flex m-2">
         <div className="flex">
-            <div className="">
-
-            </div>
-
+          <img src="../logo.svg" />
         </div>
-        
-        </>
-    )
-}
+        <div className="flex ml-auto navTitle gap-2">
+          <div>Trips</div>
+          <div>Dashboard</div>
+          <div>Explore</div>
+        </div>
+      </div>
+    </>
+  );
+};
