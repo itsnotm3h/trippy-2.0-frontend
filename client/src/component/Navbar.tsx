a9c570ca3b18498a0d85a7bc4dbbe6558841f1cc
itsnotm3h <thewhit3pages@gmail.com>
2026-04-11 00:32:35 +0800

    style:Navbar

--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -1,14 +1,27 @@
+import { Link } from 'react-router-dom';
+
 export const Navbar = () => {
   return (
     <>
-      <div className="flex m-2">
+      <div className="flex bg-white p-2 min-h-12.5">
         <div className="flex">
-          <img src="../logo.svg" />
+          <img src="../logo.svg" width={100} />
         </div>
-        <div className="flex ml-auto navTitle gap-2">
-          <div>Trips</div>
-          <div>Dashboard</div>
-          <div>Explore</div>
+        <div className="flex ml-auto navTitle gap-3 items-center">
+          <div className="flex"><Link to="/"><h4>Trips</h4></Link></div>
+          <div className="flex disabledLink"><h4>Dashboard</h4></div>
+          <div className="flex disabledLink"><h4>Explore</h4></div>
+          <div className="flex disabledLink">
+            <span className="material-symbols-outlined">
+              mail
+            </span>
+          </div>
+          <div className="flex  disabledLink">
+            <span className="material-symbols-outlined">
+              paid
+            </span>
+          </div>
+          <div className="flex"><button className="button">Login</button></div>
         </div>
       </div>
     </>
