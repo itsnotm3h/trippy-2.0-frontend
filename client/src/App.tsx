dbabea759dbcd0e555d5badaaef4f0c403686d84
itsnotm3h <thewhit3pages@gmail.com>
2026-04-11 00:35:15 +0800

    refactor: remove code smell

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,8 @@ import { Navbar } from "./component/Navbar";
 
 function App() {
   return (
-    <>
       <Navbar />
 
-    </>
   );
 }
 
--- client/src/component/Navbar.tsx
+++ client/src/component/Navbar.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <>
+    
       <div className="flex bg-white p-2 min-h-12.5">
         <div className="flex">
-          <img src="../logo.svg" width={100} />
+          <img alt="trippy-logo" src="../logo.svg" width={100} />
         </div>
         <div className="flex ml-auto navTitle gap-3 items-center">
           <div className="flex"><Link to="/"><h4>Trips</h4></Link></div>
@@ -24,6 +24,5 @@ export const Navbar = () => {
           <div className="flex"><button className="button">Login</button></div>
         </div>
       </div>
-    </>
   );
 };
